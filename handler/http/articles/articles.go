7941ee9a3368213d073b309cfc0df5a6846c63f4
Hao Kai <leonkaihao@outlook.com>
2018-11-28 12:14:35 +1100

    fix create article params check

--- handler/http/articles/articles.go
+++ handler/http/articles/articles.go
@@ -23,28 +23,7 @@ func (art *Articles) Create(w rest.ResponseWriter, r *rest.Request) {
 		rest.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if len(data.Title) == 0 {
-		rest.Error(w, "Title must exist.", http.StatusBadRequest)
-		return
-	}
-	if len(data.Title) >= 256 {
-		rest.Error(w, "Title length must be less than 255.", http.StatusBadRequest)
-		return
-	}
-	if len(data.Body) == 0 {
-		rest.Error(w, "Body must exist.", http.StatusBadRequest)
-		return
-	}
-	if len(data.Body) >= 4096 {
-		rest.Error(w, "Body length must be less than 4K.", http.StatusBadRequest)
-		return
-	}
-	for _, tag := range data.Tags {
-		if len(tag) >= 64 {
-			rest.Error(w, "tag string length must be less than 63.", http.StatusBadRequest)
-			return
-		}
-	}
+
 	//ignore data Date field from client, and generate time from server
 	data.Date = time.Now().Format("2016-09-22")
 	//protect a transaction
