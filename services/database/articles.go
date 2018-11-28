6af5bd04ae6d385c2d39f1eb2a101ec98391f4c3
Hao Kai <leonkaihao@outlook.com>
2018-11-28 12:09:30 +1100

    fix create article params check

--- services/database/articles.go
+++ services/database/articles.go
@@ -1,12 +1,39 @@
 package database
 
 import (
+	"errors"
 	"log"
 )
 
 //CreateArticle ...
 func (db *Database) CreateArticle(article *ArticleRequest) (err error) {
+	if len(article.Title) == 0 {
+		err = errors.New("Title must exist")
+		return
+	}
+	if len(article.Title) >= 256 {
+		err = errors.New("Title length must be less than 255")
+		return
+	}
+	if len(article.Body) == 0 {
+		err = errors.New("Body must exist")
+		return
+	}
+	if len(article.Body) >= 4096 {
 
+		err = errors.New("Body length must be less than 4K")
+		return
+	}
+	for _, tag := range article.Tags {
+		if len(tag) >= 64 {
+			err = errors.New("Tag string length must be less than 63")
+			return
+		}
+	}
+	if len(article.Date) >= 16 {
+		err = errors.New("Date string length must be less than 15")
+		return
+	}
 	//first insert to articles table
 	itemArticle := &articleDAO{
 		Title: article.Title,
